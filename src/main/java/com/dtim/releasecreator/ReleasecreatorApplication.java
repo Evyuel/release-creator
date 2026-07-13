fd96ad56b0c3bcb68cb9d49ac38ce85d0f7fee7d
Evyuel <drugban1994@gmail.com>
2026-07-13 13:28:19 +0300

    inital commit

---
+++ src/main/java/com/dtim/releasecreator/ReleasecreatorApplication.java
@@ -0,0 +1,13 @@
+package com.dtim.releasecreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasecreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasecreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtim/releasecreator/ReleasecreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtim.releasecreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleasecreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
