df702f17adbcc4c5e769852a8760b9093a1dc5ce
Evyuel <81313394+Evyuel@users.noreply.github.com>
2026-07-13 18:32:02 +0300

    feat: add deployment report exception

---
+++ src/main/java/com/dtim/releasecreator/exception/DeploymentReportException.java
@@ -0,0 +1,8 @@
+package com.dtim.releasecreator.exception;
+
+public class DeploymentReportException extends RuntimeException {
+
+    public DeploymentReportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
