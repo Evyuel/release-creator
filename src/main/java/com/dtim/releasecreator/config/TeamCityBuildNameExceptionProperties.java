060977b5b03a2d268cf5fceb94fc8bcdeb082451
Evyuel <drugban1994@gmail.com>
2026-07-21 10:41:01 +0300

    fix

--- src/main/java/com/dtim/releasecreator/config/TeamCityBuildNameExceptionProperties.java
+++ src/main/java/com/dtim/releasecreator/config/TeamCityBuildNameExceptionProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "integrations.build-name-expections")
+@ConfigurationProperties(prefix = "integrations.build-name-exceptions")
 public record TeamCityBuildNameExceptionProperties(
         Map<String, String> releaseProductionExceptions,
         Map<String, String> justBuildExceptions,
